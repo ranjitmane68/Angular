ab7452ed7a67735c184e89a02e6debc04af7c547
Ranjit Mane <ranjitmane68@gmail.com>
2024-03-03 14:23:49 +0530

    Added forms validation and error message

--- Angular_Forms/Angular_Forms_App/src/app/app.component.ts
+++ Angular_Forms/Angular_Forms_App/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
@@ -12,10 +12,17 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  @ViewChild('f')
+  signupForm: NgForm | undefined;
+
   title = 'Angular_Forms_App';
 
+  // onSubmit(form: NgForm) {
+  //   console.log('onSubmit is called', form);
+  //   console.log(form);
+  // }
+
   onSubmit(form: NgForm) {
-    console.log('onSubmit is called', form);
-    console.log(form);
+    console.log(this.signupForm);
   }
 }
