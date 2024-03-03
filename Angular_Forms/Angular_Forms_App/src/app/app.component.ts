df9e97ba81c130f17b1378e4dabff323c7db353c
Ranjit Mane <ranjitmane68@gmail.com>
2024-03-03 11:34:12 +0530

    Created basic Angular forms project

---
+++ Angular_Forms/Angular_Forms_App/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule, HttpClientModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'Angular_Forms_App';
+
+  onSubmit(form: NgForm) {
+    console.log('onSubmit is called', form);
+    console.log(form);
+  }
+}
