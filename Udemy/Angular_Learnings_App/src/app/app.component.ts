114c78864135c197555497a99a10821ce5e4035f
Ranjit Mane <ranjitmane68@gmail.com>
2024-04-13 22:45:18 +0530

    Implemented GET and POST methods with Google Firebase

--- Udemy/Angular_Learnings_App/src/app/app.component.ts
+++ Udemy/Angular_Learnings_App/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,20 +18,45 @@ export class AppComponent {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     // Angular HTTP Client automatically converts javascript object to JSON object
     console.log(postData);
-    this.http.post(
-      'https://ng-complete-guide-d4d7d-default-rtdb.firebaseio.com/posts',
-      postData
-    );
+    this.http
+      .post(
+        'https://ng-complete-guide-d4d7d-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
+  }
+
+  private fetchPosts() {
+    this.http
+      .get<[]>(
+        'https://ng-complete-guide-d4d7d-default-rtdb.firebaseio.com/posts.json'
+      )
+      .pipe(
+        map((responseData: any[]) => {
+          const postArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postArray;
+        })
+      )
+      .subscribe((posts) => console.log(posts));
   }
 
   onClearPosts() {
